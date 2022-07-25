463c9c3dfd830280a6edb03509443a22967abe8e
Adhisha Fernando <fernandoadisha007@gmail.com>
2022-07-25 12:13:34 +0530

    Completed code for identifying wins loses and draws Also made visual changes to look like a tik tac toe grid

--- app/src/main/java/com/example/personal_tictaktoe_learning/MainActivity.kt
+++ app/src/main/java/com/example/personal_tictaktoe_learning/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.personal_tictaktoe_learning
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 
     var p1Chance: Boolean = true
+    var gamePlayable: Boolean = true
     var drawCount : Int = 0
 
     var a1Click: Boolean = true
@@ -26,8 +26,20 @@ class MainActivity : AppCompatActivity() {
     var c2Click: Boolean = true
     var c3Click: Boolean = true
 
+    var pOneName = "Player 01"
+    var pTwoName = "Player 02"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //initializing buttons
+    }
+
     fun a1Action(view: View) {
-        if(a1Click){
+        if(a1Click && gamePlayable){
+            //btnA1.setBackgroundColor(ContextCompat.getColor(this, R.color.goGreen))
             if (p1Chance) {
                 btnA1.text = "X"
                 p1Chance = false
@@ -35,13 +47,13 @@ class MainActivity : AppCompatActivity() {
                 btnA1.text = "O"
                 p1Chance = true
             }
-            a1Click=false
             drawCount++
             winLoseCheck()
         }
+        a1Click=false
     }
     fun a2Action(view: View) {
-        if(a2Click){
+        if(a2Click && gamePlayable){
             if (p1Chance) {
                 btnA2.text = "X"
                 p1Chance = false
@@ -49,13 +61,13 @@ class MainActivity : AppCompatActivity() {
                 btnA2.text = "O"
                 p1Chance = true
             }
-            a2Click=false
             drawCount++
             winLoseCheck()
         }
+        a2Click=false
     }
     fun a3Action(view: View) {
-        if(a3Click){
+        if(a3Click && gamePlayable){
             if (p1Chance) {
                 btnA3.text = "X"
                 p1Chance = false
@@ -63,13 +75,13 @@ class MainActivity : AppCompatActivity() {
                 btnA3.text = "O"
                 p1Chance = true
             }
-            a3Click=false
             drawCount++
             winLoseCheck()
         }
+        a3Click=false
     }
     fun b1Action(view: View) {
-        if(b1Click){
+        if(b1Click && gamePlayable){
             if (p1Chance) {
                 btnB1.text = "X"
                 p1Chance = false
@@ -77,13 +89,13 @@ class MainActivity : AppCompatActivity() {
                 btnB1.text = "O"
                 p1Chance = true
             }
-            b1Click=false
             drawCount++
             winLoseCheck()
         }
+        b1Click=false
     }
     fun b2Action(view: View) {
-        if(b2Click){
+        if(b2Click && gamePlayable){
             if (p1Chance) {
                 btnB2.text = "X"
                 p1Chance = false
@@ -91,13 +103,13 @@ class MainActivity : AppCompatActivity() {
                 btnB2.text = "O"
                 p1Chance = true
             }
-            b2Click=false
             drawCount++
             winLoseCheck()
         }
+        b2Click=false
     }
     fun b3Action(view: View) {
-        if(b3Click){
+        if(b3Click && gamePlayable){
             if (p1Chance) {
                 btnB3.text = "X"
                 p1Chance = false
@@ -105,13 +117,13 @@ class MainActivity : AppCompatActivity() {
                 btnB3.text = "O"
                 p1Chance = true
             }
-            b3Click=false
             drawCount++
             winLoseCheck()
         }
+        b3Click=false
     }
     fun c1Action(view: View) {
-        if(c1Click){
+        if(c1Click && gamePlayable){
             if (p1Chance) {
                 btnC1.text = "X"
                 p1Chance = false
@@ -119,13 +131,13 @@ class MainActivity : AppCompatActivity() {
                 btnC1.text = "O"
                 p1Chance = true
             }
+            drawCount++
+            winLoseCheck()
         }
         c1Click=false
-        drawCount++
-        winLoseCheck()
     }
     fun c2Action(view: View) {
-        if(c2Click){
+        if(c2Click && gamePlayable){
             if (p1Chance) {
                 btnC2.text = "X"
                 p1Chance = false
@@ -133,13 +145,13 @@ class MainActivity : AppCompatActivity() {
                 btnC2.text = "O"
                 p1Chance = true
             }
+            drawCount++
+            winLoseCheck()
         }
         c2Click=false
-        drawCount++
-        winLoseCheck()
     }
     fun c3Action(view: View) {
-        if(c3Click){
+        if(c3Click && gamePlayable){
             if (p1Chance) {
                 btnC3.text = "X"
                 p1Chance = false
@@ -147,15 +159,97 @@ class MainActivity : AppCompatActivity() {
                 btnC3.text = "O"
                 p1Chance = true
             }
+            drawCount++
+            winLoseCheck()
         }
         c3Click=false
-        drawCount++
-        winLoseCheck()
     }
 
     private fun winLoseCheck() {
-        if(drawCount>=9){
+        var a1 = btnA1.text
+        var a2 = btnA2.text
+        var a3 = btnA3.text
+
+        var b1 = btnB1.text
+        var b2 = btnB2.text
+        var b3 = btnB3.text
+
+        var c1 = btnC1.text
+        var c2 = btnC2.text
+        var c3 = btnC3.text
+
+
+        if(drawCount>9){
             winLoseTV.text = "Draw"
         }
+        else{ //vertical 3 rows
+            if(a1==a2 && a2==a3 && a1!="") {
+                if(a1=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(b1==b2 && b2==b3 && b1!="") {
+                if(b1=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(c1==c2 && c2==c3 && c1!="") {
+                if(c1=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }  //horizontal 3 rows
+            else if(a1==b1 && c1==b1 && a1!="") {
+                if(a1=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(a2==b2 && b2==c2 && a2!="") {
+                if(a2=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(a3==b3 && b3==c3 && a3!="") {
+                if(a3=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            } //cross winnings
+            else if(a1==b2 && b2==c3 && a1!="") {
+                if(a1=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(a3==b2 && b2==c1 && a3!="") {
+                if(a3=="X")
+                    playerOneWin()
+                else
+                    playerTwoWin()
+                gamePlayable = false
+            }
+            else if(drawCount>=9){
+                winLoseTV.text = "Draw"
+            }
+        }
+    }
+
+    private fun playerOneWin() {
+        winLoseTV.text = "$pOneName Won"
+    }
+
+    private fun playerTwoWin() {
+        winLoseTV.text = "$pTwoName Won"
     }
 }
