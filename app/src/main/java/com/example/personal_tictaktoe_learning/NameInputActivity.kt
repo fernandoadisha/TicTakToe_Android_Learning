7dc6218fb590106476be6868f47d8032e22fb406
Adhisha Fernando <fernandoadisha007@gmail.com>
2022-07-27 21:40:55 +0530

    Designed simple name input form No backend or connection yet established V1.0.1

---
+++ app/src/main/java/com/example/personal_tictaktoe_learning/NameInputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.personal_tictaktoe_learning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NameInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_input)
+    }
+}
